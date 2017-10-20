6a070333f8bded9e9efbdc5907f3a40028490a93
James Soubry <james.soubry@tantalumcorporation.com>
2017-10-20 12:11:07 +0100

    move logic into functions

--- prom-check.py
+++ prom-check.py
@@ -9,6 +9,81 @@ interval = 60
 prometheus = sys.argv[1]
 alertmanager = sys.argv[2]
 
+
+def send_alert(prometheus, alertmanager):
+    """Send alert to alertmanager."""
+
+    start_time = datetime.now(timezone.utc).astimezone()
+    end_time = start_time + timedelta(minutes=interval)
+
+    print("[{}] Sending alert to {}.".format(
+            start_time,
+            alertmanager
+    ))
+
+    body = [
+        {
+            "labels": {
+                "severity": "critical",
+                "instance": prometheus,
+                "alertname": "prometheus_down"
+            },
+            "annotations": {
+                "summary": "Prometheus Down",
+                "description": "Prometheus Instance Is Down"
+            },
+            "startsAt": start_time.isoformat(),
+            "endsAt": end_time.isoformat()
+        }
+    ]
+
+    a = requests.post(
+        'http://{}/api/v1/alerts'.format(alertmanager),
+        json=body
+    )
+
+    if a.status_code == 200:
+        print("[{}] Alert sent OK.".format(
+            datetime.now(timezone.utc).astimezone()
+        ))
+        return True
+    else:
+        print("[{}] Alert failed to send.".format(
+            datetime.now(timezone.utc).astimezone()
+        ))
+        return False
+
+
+def check_prom(prometheus):
+    """Test Prometheus endpoint."""
+
+    try:
+        r = requests.get(
+            "http://{}/metrics".format(prometheus),
+            timeout=30
+        )
+
+        print("[{}] Status Code: {}".format(
+            datetime.now(timezone.utc).astimezone(),
+            r.status_code
+        ))
+
+        if r.status_code == 200:
+            return True
+        else:
+            print("[{}] {} is down.".format(
+                datetime.now(timezone.utc).astimezone(),
+                prometheus
+            ))
+            return False
+
+    except:
+        print("[{}] Failed to get prometheus endpoint in 30 seconds".format(
+            datetime.now(timezone.utc).astimezone()
+        ))
+        return False
+
+
 print(
     "Starting prom-check with prometheus: {} and alertmanager: {}".format(
         prometheus,
@@ -17,73 +92,13 @@ print(
 )
 
 while True:
-    up = False
 
     print("[{}] Testing Prometheus..".format(
         datetime.now(timezone.utc).astimezone()
     ))
 
-    try:
-        r = requests.get(
-            "http://{}/metrics".format(prometheus),
-            timeout=30
-        )
-
-        if r.status_code == 200:
-            up = True
-
-        print("[{}] Status Code: {}".format(
-            datetime.now(timezone.utc).astimezone(),
-            r.status_code
-        ))
-    except:
-        print("[{}] Failed to get prometheus endpoint in 30 seconds".format(
-            datetime.now(timezone.utc).astimezone()
-        ))
-        pass
-
-    if not up:
-
-        print(
-            "[{}] {} is down. Sending alert to {}.".format(
-                datetime.now(timezone.utc).astimezone(),
-                prometheus,
-                alertmanager
-            )
-        )
-
-        start_time = datetime.now(timezone.utc).astimezone()
-        end_time = start_time + timedelta(minutes=interval)
-
-        body = [
-            {
-                "labels": {
-                    "severity": "critical",
-                    "instance": prometheus,
-                    "alertname": "prometheus_down"
-                },
-                "annotations": {
-                    "summary": "Prometheus Down",
-                    "description": "Prometheus Instance Is Down"
-                },
-                "startsAt": start_time.isoformat(),
-                "endsAt": end_time.isoformat()
-            }
-        ]
-
-        a = requests.post(
-            'http://{}/api/v1/alerts'.format(alertmanager),
-            json=body
-        )
-
-        if a.status_code == 200:
-            print("[{}] Alert sent OK.".format(
-                datetime.now(timezone.utc).astimezone()
-            ))
-        else:
-            print("[{}] Alert failed to send.".format(
-                datetime.now(timezone.utc).astimezone()
-            ))
+    if not check_prom(prometheus):
+        send_alert(prometheus, alertmanager)
 
     print("[{}] Waiting for {} seconds..".format(
         datetime.now(timezone.utc).astimezone(),
